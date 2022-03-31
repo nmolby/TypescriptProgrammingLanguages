05c8f823f4964eeafe21e551b0116522f645b87c
nmolby <molbynathan1@gmail.com>
2022-03-31 11:38:54 -0400

    Fibonacci number is now working

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Working!");
+function calculateFib() {
+    var element = document.getElementById("fib");
+    var fibToCalculate = parseInt(element.value);
+    var elementToChange = document.getElementById("fibResult");
+    elementToChange.innerHTML = fib(fibToCalculate).toString();
+}
+function fib(x) {
+    if (x == 0) {
+        return 0;
+    }
+    else if (x == 1) {
+        return 1;
+    }
+    else if (x == 2) {
+        return 1;
+    }
+    else {
+        return fib(x - 1) + fib(x - 2);
+    }
+}
--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-console.log("Working!")
+function calculateFib() {
+    const element = <HTMLInputElement> document.getElementById("fib");
+    let fibToCalculate = parseInt(element.value)
+    const elementToChange = document.getElementById("fibResult")
+    elementToChange.innerHTML = fib(fibToCalculate).toString()
+}
+
+function fib(x: number): number {
+    if(x == 0) {
+        return 0
+    } else if (x == 1) {
+        return 1
+    } else if (x == 2) {
+        return 1
+    } else {
+        return fib(x - 1) + fib(x - 2)
+    }
+}
